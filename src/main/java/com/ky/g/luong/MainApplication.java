e1d3a2ee54045b99bd1baf1dca4fd7686eccec39
kyluong09 <kyluong09@gmail.com>
2019-04-29 02:45:05 -0500

    renamed main application class

--- src/main/java/com/ky/g/luong/MainApplication.java
+++ src/main/java/com/ky/g/luong/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootThymeleafApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootThymeleafApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 		
 		
 	}
--- src/test/java/com/ky/g/luong/MainApplicationTests.java
+++ src/test/java/com/ky/g/luong/MainApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring.boot.thymeleaf;
+package com.ky.g.luong;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootThymeleafApplicationTests {
+public class MainApplicationTests {
 
 	@Test
 	public void contextLoads() {
